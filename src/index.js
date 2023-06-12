d19c53e6079dabd6f3192796725b6b9511b287bd
Kgomotso Nacane <kgomotsonacane@gmail.com>
2023-06-12 18:46:12 +0200

    Refactor code: Improve variable names for clarity

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-/*eslint-disable*/
-import _ from 'lodash'; /*eslint-disable*/
+/* eslint-disable*/
+import _ from 'lodash';/* eslint-disable*/
 import './style.css';
-import { Functionality } from './modules/functionality';
+import { Functionality } from './modules/functionality.js';
+
 const taskContainer = document.getElementById('first-section');
 const submitButton = document.getElementById('for-button');
 const clearAllDone = document.getElementById('clear-all');
@@ -12,74 +13,72 @@ window.onload = function windowReady() {
     Functionality.newTask();
   };
 
-taskContainer.addEventListener('click', (e) => {
-  if (e.target !== null && e.target !== 'NaN' && e.target !== '') {
-    if (e.target.className === 'checkbox-class') {
-      const ids = e.target.id.replace('checkbox-', '');
-      let description = document.getElementById('d' + ids);
-      let data = Functionality.getAllTasks();
-      const index = parseInt(ids-1, 10);
-      if (data !== []) {
-        if (data[index].completed) {
-          data[index].completed = false;
-          description.style.textDecoration = 'none';
+  taskContainer.addEventListener('click', (e) => {
+    if (e.target !== null && e.target !== 'NaN' && e.target !== '') {
+      if (e.target.className === 'checkbox-class') {
+        const taskIds = e.target.id.replace('checkbox-', '');
+        const taskDescription = document.getElementById(`d${taskIds}`);
+        const taskData = Functionality.getAllTasks();
+        const index = parseInt(taskIds - 1, 10);
+        if (taskData !== []) {
+          if (taskData[index].completed) {
+            taskData[index].completed = false;
+            taskDescription.style.textDecoration = 'none';
+          } else {
+            taskData[index].completed = true;
+            taskDescription.style.textDecoration = 'line-through';
+          }
+          Functionality.updateTask(taskData);
         }
-        else {
-          data[index].completed = true;
-          description.style.textDecoration = 'line-through';
-        }
-        Functionality.updateTask(data);
       }
     }
-  }
-});
+  });
 
-taskContainer.addEventListener('click', (e) => {
-  if (e.target !== null && e.target !== 'NaN' && e.target !== '') {
-    if (e.target.className === 'editButton') {
-      const ids = e.target.id.replace('editBttn-', '');
-      let description = document.getElementById('d' + ids);
-      let data = Functionality.getAllTasks();
-      const index = parseInt(ids, 10);
-      let editInput = document.getElementById('edit-'+ ids);
-      description.style.display='none';
-      editInput.style.display='block';
-      if (editButtonStats !== false) {
-        data[index-1].description=editInput.value;
-        description.style.display='block';
-        editInput.style.display='none';
-        Functionality.updateTask(data);
-        Functionality.displayTask();
-        editButtonStats = false;
-      }else{
-        editButtonStats = true;
+  taskContainer.addEventListener('click', (e) => {
+    if (e.target !== null && e.target !== 'NaN' && e.target !== '') {
+      if (e.target.className === 'editButton') {
+        const taskIds = e.target.id.replace('editBttn-', '');
+        const taskDescription = document.getElementById(`d${taskIds}`);
+        const taskData = Functionality.getAllTasks();
+        const index = parseInt(taskIds, 10);
+        const editInput = document.getElementById(`edit-${taskIds}`);
+        taskDescription.style.display = 'none';
+        editInput.style.display = 'block';
+        if (editButtonStats !== false) {
+          taskData[index - 1].taskDescription = editInput.value;
+          taskDescription.style.display = 'block';
+          editInput.style.display = 'none';
+          Functionality.updateTask(taskData);
+          Functionality.displayTask();
+          editButtonStats = false;
+        } else {
+          editButtonStats = true;
+        }
       }
     }
-  }
-});
+  });
 
- Functionality.displayTask();
- taskContainer.addEventListener('click', (e) => {
-   if (e.target !== null && e.target !== 'NaN' && e.target !== '') {
-     if (e.target.className === 'deleteButton') {
-       const ids = e.target.id.replace('delete-', '');
-       const data = Functionality.getAllTasks();
-       const index = parseInt(ids, 10);
-       if (data !== []) {
-         Functionality.removeTask(index-1);
-       }
-     }
-   }
- });
-
- clearAllDone.addEventListener('click', (e)=> {
-  const data = Functionality.getAllTasks();
-  const storage = data.filter((todo)=>todo.completed === false);
-  for (let i = 0; i < storage.length; i++) {
-    storage[i].index = i + 1;
-  }
-  Functionality.updateTask(storage);
   Functionality.displayTask();
- });
+  taskContainer.addEventListener('click', (e) => {
+    if (e.target !== null && e.target !== 'NaN' && e.target !== '') {
+      if (e.target.className === 'deleteButton') {
+        const taskIds = e.target.id.replace('delete-', '');
+        const taskData = Functionality.getAllTasks();
+        const index = parseInt(taskIds, 10);
+        if (taskData !== []) {
+          Functionality.removeTask(index - 1);
+        }
+      }
+    }
+  });
 
+  clearAllDone.addEventListener('click', (e) => {
+    const taskData = Functionality.getAllTasks();
+    const storage = taskData.filter((todo) => todo.completed === false);
+    for (let i = 0; i < storage.length; i++) {
+      storage[i].index = i + 1;
+    }
+    Functionality.updateTask(storage);
+    Functionality.displayTask();
+  });
 };
